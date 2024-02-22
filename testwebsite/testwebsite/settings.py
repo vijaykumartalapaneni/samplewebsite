e4d2ecea0e6dfffb76f9cb9141855a40cffa0651
Mohan Talapaneni <vijaykumar21031997@gmail.com>
2024-02-21 20:25:33 -0500

    Second commit

--- testwebsite/testwebsite/settings.py
+++ testwebsite/testwebsite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=$+khn7$*%w*ff)op&=ulep!h4qc9oc(4273kk+ya^2d(@8$x0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
